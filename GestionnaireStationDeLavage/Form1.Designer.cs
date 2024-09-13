95d4769c291ea01318f7570d6dcf1da7eb47a70e
Léonid Gr <botoanasatraleonidsergueigr@gmail.com>
2024-09-13 17:00:05 +0500

    feat: mise en place du sidebar

---
+++ GestionnaireStationDeLavage/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace GestionnaireStationDeLavage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelSlide = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnMoney = new System.Windows.Forms.Button();
+            this.btnService = new System.Windows.Forms.Button();
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.btnEmployer = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.panel_logo = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panelSlide);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 706);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelSlide
+            // 
+            this.panelSlide.BackColor = System.Drawing.Color.SkyBlue;
+            this.panelSlide.Location = new System.Drawing.Point(0, 150);
+            this.panelSlide.Name = "panelSlide";
+            this.panelSlide.Size = new System.Drawing.Size(10, 45);
+            this.panelSlide.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.btnLogout);
+            this.panel2.Controls.Add(this.btnSetting);
+            this.panel2.Controls.Add(this.btnReport);
+            this.panel2.Controls.Add(this.btnMoney);
+            this.panel2.Controls.Add(this.btnService);
+            this.panel2.Controls.Add(this.btnCustomer);
+            this.panel2.Controls.Add(this.btnEmployer);
+            this.panel2.Controls.Add(this.btnDashboard);
+            this.panel2.Controls.Add(this.panel_logo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(10, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 706);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnLogout.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnLogout.Image = global::GestionnaireStationDeLavage.Properties.Resources.exit_45px;
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(0, 661);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(200, 45);
+            this.btnLogout.TabIndex = 9;
+            this.btnLogout.Text = "DECONNEXION";
+            this.btnLogout.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.BackColor = System.Drawing.Color.Transparent;
+            this.btnSetting.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSetting.FlatAppearance.BorderSize = 0;
+            this.btnSetting.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnSetting.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSetting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetting.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetting.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnSetting.Image = global::GestionnaireStationDeLavage.Properties.Resources.settings_45px;
+            this.btnSetting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetting.Location = new System.Drawing.Point(0, 420);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(200, 45);
+            this.btnSetting.TabIndex = 8;
+            this.btnSetting.Text = "PARAMETRES";
+            this.btnSetting.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSetting.UseVisualStyleBackColor = false;
+            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // btnReport
+            // 
+            this.btnReport.BackColor = System.Drawing.Color.Transparent;
+            this.btnReport.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReport.FlatAppearance.BorderSize = 0;
+            this.btnReport.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnReport.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReport.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReport.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnReport.Image = global::GestionnaireStationDeLavage.Properties.Resources.report_file_45px;
+            this.btnReport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReport.Location = new System.Drawing.Point(0, 375);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(200, 45);
+            this.btnReport.TabIndex = 7;
+            this.btnReport.Text = "RAPPORT";
+            this.btnReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReport.UseVisualStyleBackColor = false;
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // btnMoney
+            // 
+            this.btnMoney.BackColor = System.Drawing.Color.Transparent;
+            this.btnMoney.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMoney.FlatAppearance.BorderSize = 0;
+            this.btnMoney.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnMoney.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnMoney.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMoney.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoney.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnMoney.Image = global::GestionnaireStationDeLavage.Properties.Resources.money_45px;
+            this.btnMoney.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMoney.Location = new System.Drawing.Point(0, 330);
+            this.btnMoney.Name = "btnMoney";
+            this.btnMoney.Size = new System.Drawing.Size(200, 45);
+            this.btnMoney.TabIndex = 6;
+            this.btnMoney.Text = "ARGENTS";
+            this.btnMoney.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMoney.UseVisualStyleBackColor = false;
+            this.btnMoney.Click += new System.EventHandler(this.btnMoney_Click);
+            // 
+            // btnService
+            // 
+            this.btnService.BackColor = System.Drawing.Color.Transparent;
+            this.btnService.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnService.FlatAppearance.BorderSize = 0;
+            this.btnService.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnService.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnService.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnService.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnService.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnService.Image = global::GestionnaireStationDeLavage.Properties.Resources.service_45px;
+            this.btnService.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnService.Location = new System.Drawing.Point(0, 285);
+            this.btnService.Name = "btnService";
+            this.btnService.Size = new System.Drawing.Size(200, 45);
+            this.btnService.TabIndex = 5;
+            this.btnService.Text = "SERVICES";
+            this.btnService.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnService.UseVisualStyleBackColor = false;
+            this.btnService.Click += new System.EventHandler(this.btnService_Click);
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.BackColor = System.Drawing.Color.Transparent;
+            this.btnCustomer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCustomer.FlatAppearance.BorderSize = 0;
+            this.btnCustomer.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnCustomer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCustomer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomer.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomer.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnCustomer.Image = global::GestionnaireStationDeLavage.Properties.Resources.customer_insight_45px;
+            this.btnCustomer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomer.Location = new System.Drawing.Point(0, 240);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(200, 45);
+            this.btnCustomer.TabIndex = 4;
+            this.btnCustomer.Text = "CLIENTS";
+            this.btnCustomer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCustomer.UseVisualStyleBackColor = false;
+            this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
+            // 
+            // btnEmployer
+            // 
+            this.btnEmployer.BackColor = System.Drawing.Color.Transparent;
+            this.btnEmployer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmployer.FlatAppearance.BorderSize = 0;
+            this.btnEmployer.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnEmployer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnEmployer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployer.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployer.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnEmployer.Image = global::GestionnaireStationDeLavage.Properties.Resources.name_tag_45px;
+            this.btnEmployer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmployer.Location = new System.Drawing.Point(0, 195);
+            this.btnEmployer.Name = "btnEmployer";
+            this.btnEmployer.Size = new System.Drawing.Size(200, 45);
+            this.btnEmployer.TabIndex = 3;
+            this.btnEmployer.Text = "EMPLOYEE";
+            this.btnEmployer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEmployer.UseVisualStyleBackColor = false;
+            this.btnEmployer.Click += new System.EventHandler(this.btnEmployer_Click);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.Transparent;
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnDashboard.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnDashboard.Image = global::GestionnaireStationDeLavage.Properties.Resources.dashboard_layout_45px;
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 150);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(200, 45);
+            this.btnDashboard.TabIndex = 2;
+            this.btnDashboard.Text = "DASHBOARD";
+            this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel_logo
+            // 
+            this.panel_logo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_logo.Location = new System.Drawing.Point(0, 0);
+            this.panel_logo.Name = "panel_logo";
+            this.panel_logo.Size = new System.Drawing.Size(200, 150);
+            this.panel_logo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 706);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Panel panel_logo;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.Button btnMoney;
+        private System.Windows.Forms.Button btnService;
+        private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.Button btnEmployer;
+        private System.Windows.Forms.Panel panelSlide;
+    }
+}
+
---
+++ GestionnaireStationDeLavage/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionnaireStationDeLavage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnDashboard.Height;
+            panelSlide.Top = btnDashboard.Top;
+        }
+
+        private void btnEmployer_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnEmployer.Height;
+            panelSlide.Top = btnEmployer.Top;
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnCustomer.Height;
+            panelSlide.Top = btnCustomer.Top;
+        }
+
+        private void btnService_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnService.Height;
+            panelSlide.Top = btnService.Top;
+        }
+
+        private void btnMoney_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnMoney.Height;
+            panelSlide.Top = btnMoney.Top;
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnReport.Height;
+            panelSlide.Top = btnReport.Top;
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnSetting.Height;
+            panelSlide.Top = btnSetting.Top;
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            panelSlide.Height = btnLogout.Height;
+            panelSlide.Top = btnLogout.Top;
+        }
+    }
+}
